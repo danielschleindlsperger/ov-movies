1d3ff36689731349623b9196dbcadfae73bac381
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-01-20 21:40:36 +0100

    insert multiple movies

--- src/ov_movies/db/sql/movies.sql
+++ src/ov_movies/db/sql/movies.sql
@@ -1,7 +1,16 @@
 -- src/ov_movies/db/sql/movies.sql
 
--- A :result value of :n below will return affected rows:
--- :name insert-movie :! :n
--- :doc Insert a single movie returning affected row count
+-- :name insert-movie :returning-execute
+-- :doc Insert a single movie returning affected row's id
 INSERT INTO movies (id, name, poster_url)
-VALUES (:id, :name, :poster-url) ON CONFLICT (id) DO NOTHING;
+VALUES (:id, :name, :poster-url)
+ON CONFLICT (id) DO NOTHING
+RETURNING *;
+
+-- A :result value of :n below will return affected rows:
+-- :name insert-movies :returning-execute
+-- :doc Insert movies returning affected rows' id
+INSERT INTO movies (id, name, poster_url)
+VALUES :t*:movies
+ON CONFLICT (id) DO NOTHING
+RETURNING *;
