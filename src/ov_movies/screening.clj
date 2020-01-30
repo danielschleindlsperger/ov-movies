b4ab5c73e5d4508591ca8c378d1f6c83cd84494e
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-01-30 01:17:21 +0100

    better spec for screening date

--- src/ov_movies/screening.clj
+++ src/ov_movies/screening.clj
@@ -1,14 +1,13 @@
 (ns ov_movies.screening
   (:require [clojure.spec.alpha :as s]
-            [clojure.spec.gen.alpha :as gen]
-            [clojure.string :as str])
-  (:import [java.time OffsetDateTime ZoneOffset ZonedDateTime]))
+            [clojure.spec.gen.alpha :as gen])
+  (:import [java.time OffsetDateTime ZonedDateTime]))
 
 
 (s/def ::id (s/with-gen (s/and string? #(-> % count (> 2)))
                         #(gen/string-alphanumeric)))
 
-(s/def ::date (s/with-gen any?
+(s/def ::date (s/with-gen (partial type OffsetDateTime)
                           ;; TODO: generate, don't use "now()"
                           #(gen/fmap (fn [_] (.toOffsetDateTime (ZonedDateTime/now))) (s/gen any?))))
 
@@ -16,5 +15,3 @@
 
 (s/def ::screening (s/keys :req [::id ::date]
                            :opt [::movie_id]))
-
-(gen/sample (s/gen ::screening) 2)
