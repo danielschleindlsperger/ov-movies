b8fc15038db500e9ec808d947636c38504d1f37c
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-02-17 21:06:54 +0100

    add basic duct/integrant structure

---
+++ dev/src/dev.clj
@@ -0,0 +1,37 @@
+(ns dev
+  (:refer-clojure :exclude [test])
+  (:require [clojure.repl :refer :all]
+            [fipp.edn :refer [pprint]]
+            [clojure.tools.namespace.repl :refer [refresh]]
+            [clojure.java.io :as io]
+            [duct.core :as duct]
+            [duct.core.repl :as duct-repl :refer [auto-reset]]
+            [eftest.runner :as eftest]
+            [integrant.core :as ig]
+            [integrant.repl :refer [clear halt go init prep reset]]
+            [integrant.repl.state :refer [config system]]
+            [clojure.java.jdbc :as jdbc]))
+
+(duct/load-hierarchy)
+
+(defn read-config []
+  (duct/read-config (io/resource "ov_movies/config.edn")))
+
+(defn test []
+  (eftest/run-tests (eftest/find-tests "test")))
+
+(def profiles
+  [:duct.profile/dev :duct.profile/local])
+
+(clojure.tools.namespace.repl/set-refresh-dirs "dev/src" "src" "test")
+
+(when (io/resource "local.clj")
+  (load "local"))
+
+(integrant.repl/set-prep! #(duct/prep-config (read-config) profiles))
+
+(defn get-db [] (:duct.database.sql/hikaricp system))
+
+(defn sql [sql-params]
+  (let [db (:spec (:duct.database.sql/hikaricp system))]
+    (jdbc/query db sql-params)))
---
+++ src/ov_movies/main.clj
@@ -0,0 +1,14 @@
+(ns ov-movies.main
+  (:gen-class)
+  (:require [duct.core :as duct]
+            #_hello-duct.boundary.movie
+            #_hello-duct.handler.example))
+
+(duct/load-hierarchy)
+
+(defn -main [& args]
+  (let [keys     (or (duct/parse-keys args) [:duct/daemon])
+        profiles [:duct.profile/prod]]
+    (-> (duct/resource "ov_movies/config.edn")
+        (duct/read-config)
+        (duct/exec-config profiles keys))))
