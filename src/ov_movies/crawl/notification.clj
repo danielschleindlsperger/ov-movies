1cb11e91b034bf207943b3fa3405ea5500d23c76
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-09-15 10:50:31 +0200

    remove clutter from notification message

--- src/ov_movies/crawl/notification.clj
+++ src/ov_movies/crawl/notification.clj
@@ -1,6 +1,5 @@
 (ns ov-movies.crawl.notification
-  (:require [ov-movies.util :refer [format-date]]
-            [ov-movies.config :refer [config]]
+  (:require [ov-movies.config :refer [config]]
             [org.httpkit.client :as client]
             [clojure.string :as str]
             [taoensso.timbre :as log]))
@@ -8,16 +7,10 @@
 ;; https://pushover.net/api
 (def endpoint "https://api.pushover.net/1/messages.json")
 
-(defn format-screening [screening] (-> screening :date format-date))
-(defn format-screenings [screenings] (str/join "\n" (map format-screening (sort-by :date screenings))))
-
 (defn format-message [movies]
   (str/join "\n\n" (map (fn [movie]
-                          (let [screenings (format-screenings (:screenings movie))
-                                link (format "<a href=\"%s/blacklist/%s\">IGNORE</a>" (-> config :server :base-url) (:id movie))]
-                            (str/join "\n" [(str "<b>" (:title movie) ":</b>")
-                                            screenings
-                                            link]))) movies)))
+                          (str/join "\n" [(str "<b>" (:title movie) ":</b>")
+                                          (str/join "\n" (distinct (map :cinema (:screenings movie))))])) movies)))
 
 (defn send-message [params api-key user-key]
   {:pre [(some? api-key) (some? user-key)]}
