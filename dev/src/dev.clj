7f10b05ac37ba279c0e83aad4c8f0fe8616857e2
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-09-24 10:14:02 +0200

    increase dx: add vscode scripts, run migrations and crawler from repl

--- dev/src/dev.clj
+++ dev/src/dev.clj
@@ -2,8 +2,10 @@
   (:refer-clojure :exclude [test])
   (:require [eftest.runner :as eftest]
             [next.jdbc :as jdbc]
-            [ov-movies.database :refer [db]]
             [org.httpkit.client :refer [request]]
+            [ov-movies.database :as database]
+            [ov-movies.config :refer [config]]
+            [ov-movies.crawl.crawler :as crawler]
             [ov-movies.web-server :refer [restart-server]]))
 
 (defn restart []
@@ -12,8 +14,17 @@
 (defn test []
   (eftest/run-tests (eftest/find-tests "test")))
 
-(defn query [stmt] (jdbc/execute! db [stmt]))
+(defn crawl! []
+  (let [movie-db-api-key (get-in config [:movie-db :api-key])]
+    (crawler/crawl! database/db movie-db-api-key)))
+
+(defn query [stmt] (jdbc/execute! database/db [stmt]))
 
 (defn http [url] (deref (request {:url url}) 10000 "Timed out after 10000ms"))
 
+;; run once initially when jacking-in
 (restart)
+
+(comment
+  (database/migrate!)
+  (crawl!))
