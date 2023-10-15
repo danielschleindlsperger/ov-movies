a70b1d32a1fb3024e54887bdf0bdc0bba7ae1674
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2023-10-15 20:03:32 +0200

    fix db connection string for newer clojure jdbc wrapper

--- src/ov_movies/database.clj
+++ src/ov_movies/database.clj
@@ -31,9 +31,9 @@
 ; In the future we can use a connection pool here.
 ; For now we just return the database uri that can be passed to jdbc as is.
 (def db
-  (-> config
-      :database
-      :connection-uri))
+  {:jdbcUrl (-> config
+                :database
+                :connection-uri)})
 (defn- migratus-config
   []
   {:store :database,
